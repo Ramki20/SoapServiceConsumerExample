7c3c1925dd3707f6f4b2dbed9172ce934b345707
Ramki20 <sivaram.natarajan@yahoo.com>
2025-07-06 15:25:52 -0500

    fix - test case failures

--- src/main/java/com/example/service/SimpleAuthorizationSoapClient.java
+++ src/main/java/com/example/service/SimpleAuthorizationSoapClient.java
@@ -20,13 +20,14 @@ import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Working SOAP client using WebServiceTemplate with correct method signatures
+ * Corrected SOAP client that avoids double envelope wrapping
  */
 @Service
 public class SimpleAuthorizationSoapClient {
@@ -54,17 +55,8 @@ public class SimpleAuthorizationSoapClient {
      */
     public boolean isHealthy() {
         try {
-            String soapRequest = """
-                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
-                                  xmlns:web="http://web.service.eas.citso.fsa.usda.gov">
-                   <soapenv:Header/>
-                   <soapenv:Body>
-                      <web:isHealthy/>
-                   </soapenv:Body>
-                </soapenv:Envelope>
-                """;
-
-            String response = sendSoapRequest(soapRequest);
+            Document requestDoc = createSoapBodyContent("isHealthy", null);
+            String response = sendSoapRequest(requestDoc);
             
             // Parse response to extract boolean value
             DocumentBuilder builder = docBuilderFactory.newDocumentBuilder();
@@ -85,24 +77,8 @@ public class SimpleAuthorizationSoapClient {
      */
     public UserIdentityDto findMatchingUserIdentity(String usdaEauthId) {
         try {
-            String soapRequest = String.format("""
-                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
-                                  xmlns:web="http://web.service.eas.citso.fsa.usda.gov">
-                   <soapenv:Header/>
-                   <soapenv:Body>
-                      <web:findMatchingUserIdentity>
-                         <arg0>
-                             <web:MapEntry>
-                                 <web:Key>usda_eauth_id</web:Key>
-                                 <web:Value>%s</web:Value>
-                             </web:MapEntry>
-                         </arg0>
-                      </web:findMatchingUserIdentity>
-                   </soapenv:Body>
-                </soapenv:Envelope>
-                """, usdaEauthId);
-
-            String response = sendSoapRequest(soapRequest);
+            Document requestDoc = createFindMatchingUserIdentityRequest(usdaEauthId);
+            String response = sendSoapRequest(requestDoc);
             return parseUserIdentity(response);
         } catch (Exception e) {
             logger.error("Error finding matching user identity for eauth ID: {}", usdaEauthId, e);
@@ -115,22 +91,8 @@ public class SimpleAuthorizationSoapClient {
      */
     public List<String> findOfficesByEauthId(String usdaEauthId, String officeType) {
         try {
-            String soapRequest = String.format("""
-                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
-                                  xmlns:web="http://web.service.eas.citso.fsa.usda.gov">
-                   <soapenv:Header/>
-                   <soapenv:Body>
-                      <web:findOfficesByEauthId>
-                         <arg0>
-                             <web:UsdaEauthId>%s</web:UsdaEauthId>
-                             <web:OfficeType>%s</web:OfficeType>
-                         </arg0>
-                      </web:findOfficesByEauthId>
-                   </soapenv:Body>
-                </soapenv:Envelope>
-                """, usdaEauthId, officeType);
-
-            String response = sendSoapRequest(soapRequest);
+            Document requestDoc = createFindOfficesByEauthIdRequest(usdaEauthId, officeType);
+            String response = sendSoapRequest(requestDoc);
             return parseListValues(response, "//ns2:Offices/ns2:ListValue");
         } catch (Exception e) {
             logger.error("Error finding offices for eauth ID: {} and office type: {}", usdaEauthId, officeType, e);
@@ -143,28 +105,8 @@ public class SimpleAuthorizationSoapClient {
      */
     public UserRolesResponseDto getUserRoles(UserIdentityDto userIdentityDto) {
         try {
-            String soapRequest = String.format("""
-                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
-                                  xmlns:web="http://web.service.eas.citso.fsa.usda.gov">
-                   <soapenv:Header/>
-                   <soapenv:Body>
-                      <web:getUserRoles>
-                         <arg0>
-                            <web:UserIdentity>
-                               <AuthenticationSystemIdentifier>%s</AuthenticationSystemIdentifier>
-                               <AuthorizationSystemIdentifier>%s</AuthorizationSystemIdentifier>
-                               <UserLoginName>%s</UserLoginName>
-                            </web:UserIdentity>
-                         </arg0>
-                      </web:getUserRoles>
-                   </soapenv:Body>
-                </soapenv:Envelope>
-                """, 
-                userIdentityDto.getAuthenticationSystemIdentifier(),
-                userIdentityDto.getAuthorizationSystemIdentifier(),
-                userIdentityDto.getUserLoginName());
-
-            String response = sendSoapRequest(soapRequest);
+            Document requestDoc = createGetUserRolesRequest(userIdentityDto);
+            String response = sendSoapRequest(requestDoc);
             return parseUserRoles(response);
         } catch (Exception e) {
             logger.error("Error getting user roles for user: {}", userIdentityDto.getUserLoginName(), e);
@@ -172,21 +114,123 @@ public class SimpleAuthorizationSoapClient {
         }
     }
 
-    private String sendSoapRequest(String soapRequest) {
+    private Document createSoapBodyContent(String operation, Element arg0) throws Exception {
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        // Create only the operation element (not full SOAP envelope)
+        Element operationElement = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", operation);
+        doc.appendChild(operationElement);
+
+        if (arg0 != null) {
+            // Import the arg0 element to this document
+            Node importedArg0 = doc.importNode(arg0, true);
+            operationElement.appendChild(importedArg0);
+        }
+
+        return doc;
+    }
+
+    private Document createFindMatchingUserIdentityRequest(String usdaEauthId) throws Exception {
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        // Create operation element
+        Element operationElement = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "findMatchingUserIdentity");
+        doc.appendChild(operationElement);
+
+        // Create arg0
+        Element arg0 = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "arg0");
+        operationElement.appendChild(arg0);
+
+        // Create MapEntry
+        Element mapEntry = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "MapEntry");
+        arg0.appendChild(mapEntry);
+
+        // Create Key and Value
+        Element key = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "Key");
+        key.setTextContent("usda_eauth_id");
+        mapEntry.appendChild(key);
+
+        Element value = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "Value");
+        value.setTextContent(usdaEauthId);
+        mapEntry.appendChild(value);
+
+        return doc;
+    }
+
+    private Document createFindOfficesByEauthIdRequest(String usdaEauthId, String officeType) throws Exception {
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        // Create operation element
+        Element operationElement = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "findOfficesByEauthId");
+        doc.appendChild(operationElement);
+
+        // Create arg0
+        Element arg0 = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "arg0");
+        operationElement.appendChild(arg0);
+
+        // Create UsdaEauthId
+        Element eauthIdElement = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "UsdaEauthId");
+        eauthIdElement.setTextContent(usdaEauthId);
+        arg0.appendChild(eauthIdElement);
+
+        // Create OfficeType
+        Element officeTypeElement = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "OfficeType");
+        officeTypeElement.setTextContent(officeType);
+        arg0.appendChild(officeTypeElement);
+
+        return doc;
+    }
+
+    private Document createGetUserRolesRequest(UserIdentityDto userIdentityDto) throws Exception {
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        // Create operation element
+        Element operationElement = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "getUserRoles");
+        doc.appendChild(operationElement);
+
+        // Create arg0
+        Element arg0 = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "arg0");
+        operationElement.appendChild(arg0);
+
+        // Create UserIdentity
+        Element userIdentityElement = doc.createElementNS("http://web.service.eas.citso.fsa.usda.gov", "UserIdentity");
+        arg0.appendChild(userIdentityElement);
+
+        // Create UserIdentity fields
+        Element authSysId = doc.createElement("AuthenticationSystemIdentifier");
+        authSysId.setTextContent(userIdentityDto.getAuthenticationSystemIdentifier());
+        userIdentityElement.appendChild(authSysId);
+
+        Element authzSysId = doc.createElement("AuthorizationSystemIdentifier");
+        authzSysId.setTextContent(userIdentityDto.getAuthorizationSystemIdentifier());
+        userIdentityElement.appendChild(authzSysId);
+
+        Element loginName = doc.createElement("UserLoginName");
+        loginName.setTextContent(userIdentityDto.getUserLoginName());
+        userIdentityElement.appendChild(loginName);
+
+        return doc;
+    }
+
+    private String sendSoapRequest(Document requestDoc) {
         try {
             logger.debug("Sending SOAP request to: {}", serviceUrl);
-            logger.debug("Request: {}", soapRequest);
+            
+            // Log the body content only (not full envelope)
+            if (logger.isDebugEnabled()) {
+                logger.debug("Request body content: {}", documentToString(requestDoc));
+            }
 
             webServiceTemplate.setDefaultUri(serviceUrl);
 
-            // Convert string to Document
-            DocumentBuilder builder = docBuilderFactory.newDocumentBuilder();
-            Document requestDoc = builder.parse(new java.io.ByteArrayInputStream(soapRequest.getBytes()));
-
-            // Create DOMSource from request
+            // Create DOMSource from request body content
             DOMSource requestSource = new DOMSource(requestDoc);
 
-            // Send request and receive response using the correct method signature
+            // Send request - WebServiceTemplate will wrap it in proper SOAP envelope
             DOMSource responseSource = webServiceTemplate.sendSourceAndReceive(
                 serviceUrl,
                 requestSource,
@@ -196,18 +240,27 @@ public class SimpleAuthorizationSoapClient {
 
             // Convert response to string
             Document responseDoc = (Document) responseSource.getNode();
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-            
-            StringWriter writer = new StringWriter();
-            transformer.transform(new DOMSource(responseDoc), new StreamResult(writer));
-            String response = writer.toString();
+            String response = documentToString(responseDoc);
             
             logger.debug("Response: {}", response);
             return response;
         } catch (Exception e) {
             logger.error("Error sending SOAP request", e);
             throw new RuntimeException("Failed to send SOAP request", e);
+        }
+    }
+
+    private String documentToString(Document doc) {
+        try {
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(doc), new StreamResult(writer));
+            return writer.toString();
+        } catch (Exception e) {
+            return "Error converting document to string: " + e.getMessage();
         }
     }
 
